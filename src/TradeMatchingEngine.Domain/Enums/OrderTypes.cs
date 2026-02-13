e9a2545b518c3641d31447fe23cc102ce7ee1ce7
Stijn <stijn.maris@me.com>
2026-02-13 23:56:49 +0000

    Initial commit after setup

---
+++ src/TradeMatchingEngine.Domain/Enums/OrderTypes.cs
@@ -0,0 +1,7 @@
+namespace TradeMatchingEngine.Domain;
+
+public enum OrderType
+{
+    Buy,
+    Sell
+}
---
+++ tests/TradeMatchingEngine.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TradeMatchingEngine.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
