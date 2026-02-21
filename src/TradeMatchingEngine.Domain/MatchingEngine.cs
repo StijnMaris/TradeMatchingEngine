9d412771b1bd2ed51caf5fd17b6a8641e8b650e4
Stijn <stijn.maris@me.com>
2026-02-21 18:22:06 +0000

    Implement MatchingEngine and enhance Order class with static creation method Tried to use a record but class seems more sensible since perfect trades with matching quantity are the least likely outcome

---
+++ src/TradeMatchingEngine.Domain/MatchingEngine.cs
@@ -0,0 +1,105 @@
+
+using TradeMatchingEngine.Domain.Models;
+using TradeMatchingEngine.Domain.Enums;
+
+namespace TradeMatchingEngine.Domain;
+
+public class MatchingEngine
+{  
+    private readonly OrderBook orderBook = new OrderBook();
+
+    private List<Trade> tradeRecord { get; } = new List<Trade>();
+
+    public IReadOnlyList<Trade> TradeHistory => tradeRecord.AsReadOnly();
+
+    public void ProcessOrder(Order order)
+    {
+        if (order == null || order.quantity <= 0)
+        {
+            throw new ArgumentException("Order must be valid && quantity must be greater than zero.");
+        }
+
+        if (order.orderType == OrderType.Buy)
+        {
+            ProcessBuyOrder(order);
+        }
+        else
+        {
+            ProcessSellOrder(order);
+        }
+    }
+
+    private void ProcessBuyOrder(Order buyOrder)
+    {
+       Order? bestSellOrder = orderBook.GetBestSellOrder();
+
+        while(bestSellOrder != null && !buyOrder.IsEmpty() && buyOrder.price >= bestSellOrder.price)
+        {
+            decimal tradeQuantity = Math.Min(buyOrder.quantity, bestSellOrder.quantity);
+            decimal tradePrice = bestSellOrder.price;
+            
+            Trade trade = new Trade(Guid.NewGuid(), buyOrder.id, bestSellOrder.id, tradeQuantity, tradePrice, DateTime.UtcNow);
+            tradeRecord.Add(trade);
+
+            if (buyOrder.quantity > tradeQuantity)
+            {
+                buyOrder.Fill(tradeQuantity);
+
+                orderBook.CancelOrder(bestSellOrder);
+                bestSellOrder = orderBook.GetBestSellOrder();
+            }
+            else if (buyOrder.quantity == tradeQuantity)
+            {
+                buyOrder.Clear();
+                orderBook.CancelOrder(bestSellOrder);
+            }
+            else
+            {
+                buyOrder.Clear();
+                bestSellOrder.Fill(tradeQuantity);
+            }    
+        }
+
+        if(!buyOrder.IsEmpty())
+        {
+            orderBook.AddOrder(buyOrder);
+        } 
+    }
+
+    private void ProcessSellOrder(Order sellOrder)
+    {
+        Order? bestBuyOrder = orderBook.GetBestBuyOrder();
+
+        while(bestBuyOrder != null && !sellOrder.IsEmpty() && sellOrder.price <= bestBuyOrder.price)
+        {
+            decimal tradeQuantity = Math.Min(sellOrder.quantity, bestBuyOrder.quantity);
+            decimal tradePrice = bestBuyOrder.price;
+            
+            Trade trade = new Trade(Guid.NewGuid(), bestBuyOrder.id, sellOrder.id, tradeQuantity, tradePrice, DateTime.UtcNow);
+            tradeRecord.Add(trade);
+
+            if (sellOrder.quantity > tradeQuantity)
+            {
+                sellOrder.Fill(tradeQuantity);
+
+                orderBook.CancelOrder(bestBuyOrder);
+                bestBuyOrder = orderBook.GetBestBuyOrder();
+            }
+            else if (sellOrder.quantity == tradeQuantity)
+            {
+                sellOrder.Clear();
+                orderBook.CancelOrder(bestBuyOrder);
+            }
+            else
+            {
+                sellOrder.Clear();
+                bestBuyOrder.Fill(tradeQuantity);
+            }    
+        }
+
+        if(!sellOrder.IsEmpty())
+        {
+            orderBook.AddOrder(sellOrder);
+        }
+    }
+}
--- src/TradeMatchingEngine.Domain/Models/Order.cs
+++ src/TradeMatchingEngine.Domain/Models/Order.cs
@@ -2,11 +2,46 @@
 using TradeMatchingEngine.Domain.Enums;
 
 namespace TradeMatchingEngine.Domain.Models;
-public record Order
-(
-    Guid id,
-    OrderType orderType,
-    decimal price,
-    decimal quantity,
-    DateTime timestamp
-);
+public class Order
+{
+
+    public static Order Create(OrderType orderType, decimal price, decimal quantity)
+    {
+        if(price <= 0 || price > decimal.MaxValue || quantity <= 0 || quantity > decimal.MaxValue)
+        {
+            throw new ArgumentException("Price and quantity must be greater than zero.");
+        }
+
+        return new Order(Guid.NewGuid(), orderType, price, quantity, DateTime.UtcNow);
+    }
+
+    private Order(Guid id, OrderType orderType, decimal price, decimal quantity, DateTime timestamp)
+    {
+        this.id = id;
+        this.orderType = orderType;
+        this.price = price;
+        this.quantity = quantity;
+        this.timestamp = timestamp;
+    }
+
+    public void Fill(decimal usedQuantity)
+    {
+        quantity = Math.Max(0, quantity - usedQuantity);
+    }
+
+    public void Clear() 
+    {
+        quantity = 0;
+    }
+
+    public bool IsEmpty() 
+    {
+        return quantity == 0;
+    }
+
+    public Guid id { get; init; }
+    public OrderType orderType { get; init; }
+    public decimal price { get; init; }
+    public decimal quantity { get; private set; }
+    public DateTime timestamp { get; init; }
+};
